19d5c5b74049e8a8478888ce63415206211c38da
Brian Carlson <briancarlson6174@gmail.com>
2018-02-17 14:15:05 -0500

    plz fix

---
+++ blast.py
@@ -0,0 +1,53 @@
+import subprocess
+
+
+def create_blast_database(fasta_str, save_path):
+    """
+
+    :param fasta_str:
+    :param save_path:
+    :return:
+    """
+    with open(save_path + 'fasta.txt', 'w') as f:
+        f.write(fasta_str)
+        f.close()
+    database_filename = save_path + '_database'
+
+    # creation of the makeblastdb command line
+    makeblastdb_cline = subprocess.Popen(['C:/Users/Christian/Desktop/RESearch/ncbi-blast-2.7.1+/bin/makeblastdb.exe',
+                                          '-in', save_path + 'fasta.txt', '-parse_seqids', '-dbtype', 'nucl', '-out',
+                                          database_filename], stdout=subprocess.PIPE)
+
+    # calls the makeblastdb command line and cleans up unnecessary files
+    makeblastdb_cline.communicate()
+
+    return database_filename
+
+
+def blast_n(fasta_str, save_path, database_filename):
+    """
+
+    :param fasta_str:
+    :param save_path:
+    :param database_filename:
+    :return:
+    """
+    with open(save_path, 'w') as f:
+        f.write(fasta_str)
+        f.close()
+
+    # generate unique filename for query
+    blast_output_file_path = save_path[:-4] + '.xml'
+
+    print(blast_output_file_path)
+    print(save_path)
+
+    # creation of the blast command line
+    blastp_cline = subprocess.Popen(['C:/Users/Christian/Desktop/RESearch/ncbi-blast-2.7.1+/bin/blastn.exe', '-task',
+                                     'blastn-short', '-out', blast_output_file_path, '-outfmt', '5', '-query',
+                                     save_path, '-db', database_filename], stdout=subprocess.PIPE)
+
+    # calling of the blast command line and cleans up unnecessary files
+    blastp_cline.communicate()
+
+    return blast_output_file_path
---
+++ main.py
@@ -0,0 +1,72 @@
+import xml.etree.ElementTree as et
+import re
+
+
+def xml_parse():
+    iters = et.parse("fasta2.xml").getroot().find(
+        'BlastOutput_iterations').find('Iteration')
+
+    hits = iters.find('Iteration_hits').findall('Hit')
+    results = []
+    for r in hits:
+        result = r.find('Hit_hsps').find('Hsp').find('Hsp_hseq')
+        gene = r.find('Hit_id').text
+        results.append((gene, result.text))
+    return results
+
+
+def gene_search(seq, pat, end=0):
+    results = []
+    if end != 0:
+        seq = seq[:end+1]
+    pat = re.compile(pat)
+    return len(pat.findall(seq))
+
+
+def iterate(src, dst):
+    ''' 
+    Returns index of last letter before cut in genome should be made,
+    returns None of either string hits the end. 
+    '''
+    base_pairs = 0
+    wrong_pairs = 0
+    bp_threshold = 5
+    error_threshold = 0.25
+
+    for x, y in zip(src, dst):
+        base_pairs += 1
+        if x != y:
+            wrong_pairs += 1
+            if (base_pairs >= bp_threshold and
+                    wrong_pairs / base_pairs >= error_threshold):
+                return base_pairs - 1
+    return None
+
+
+def cluster_enzymes(li):
+    pairs = ['A^A', 'A^C', 'A^G', 'A^T', 'C^A', 'C^C', 'C^G', 'C^T',
+             'G^A', 'G^C', 'G^G', 'G^T', 'T^A', 'T^C', 'T^G', 'T^T']
+    ends = ['A^', 'C^', 'G^', 'T^']
+    starts = ['^A', '^C', '^G', '^T']
+    d = {}
+    for pair in pairs:
+        d[pair] = []
+    for s in starts:
+        d[s] = []
+    for e in ends:
+        d[e] = []
+    for enzyme in li:
+        enzyme = enzyme['pattern']
+        for pair in pairs:
+            if pair in enzyme:
+                d[pair].append(enzyme)
+                break
+        for start in starts:
+            if enzyme.startswith(start):
+                d[start].append(enzyme)
+                break
+        for end in ends:
+            if enzyme.endswith(end):
+                d[end].append(enzyme)
+                break
+    return d
