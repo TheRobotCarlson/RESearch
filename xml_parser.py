0490097bc108ebb95eca96b43d48edd8c25acf53
Zach Silver <zsilver96@gmail.com>
2018-02-17 00:17:09 -0500

    xml_parser

--- xml_parser.py
+++ xml_parser.py
@@ -1,1 +1,6 @@
 import xml.etree.ElementTree as et
+
+root = et.parse("test.xml").getroot()
+stats = root.find('BlastOutput_iterations').find(
+    'Iteration').find('Iteration_stat')
+print(stats.text)
