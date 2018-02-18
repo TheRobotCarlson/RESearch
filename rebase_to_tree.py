99a6932951b74770c68a109d5b688b064e7174b1
pryo <2012213057@bupt.edu.cn>
2018-02-17 21:20:50 -0500

    bug fixed for tree

--- rebase_to_tree.py
+++ rebase_to_tree.py
@@ -1,9 +1,9 @@
 from py2neo import Node, Relationship, Graph
-
+import sys
 
 def expand(root,graph):
-    maxleft=20
-    maxright=20
+    maxleft=5
+    maxright=5
     #if root in DB
     query = graph.find_one("rebase_enzyme",property_key="pattern",property_value=root["pattern"])
     if query:
@@ -12,24 +12,29 @@ def expand(root,graph):
         root["name"]= query["name"]
     root_pattern_left =root["pattern"].split('^')[0]
     root_pattern_right = root["pattern"].split('^')[1]
-    if not (root_pattern_left>maxleft and root_pattern_right>maxright):
+    if (len(root_pattern_left)<maxleft and len(root_pattern_right)<maxright):
 
         for appendage in ['A', 'T', 'G', 'C']:
             temp_node_left = Node("tree_node")
             temp_pattern_left = appendage + root["pattern"]
             temp_node_left["pattern"] = temp_pattern_left
-            graph.create(temp_node_left, (root, "containedby", 0))
-
+            relationship_left = Relationship(root ,"containedby",temp_node_left)
+            #graph.create(temp_node_left, relationship_left)
+            graph.create(temp_node_left)
+            graph.create(relationship_left)
             temp_node_right = Node("tree_node")
             temp_pattern_right = root["pattern"]+appendage
             temp_node_right["pattern"] = temp_pattern_right
-            graph.create(temp_node_right, (root, "containedby", 0))
+            relationship_right = Relationship(root, "containedby", temp_node_right)
+            graph.create(temp_node_right)
+            graph.create(relationship_right)
             expand(temp_node_left,graph)
             expand(temp_node_right,graph)
 
 
 
 def run():
+    sys.setrecursionlimit(100000)
     DB_graph = Graph(password="pass")
     PREMU = ["A^T","A^G","A^C","T^G","T^C","G^C","T^A","G^A","C^A","G^T","C^T","C^G"]
     roots = [Node("tree_node") for p in PREMU]
